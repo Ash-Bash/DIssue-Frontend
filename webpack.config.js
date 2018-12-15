859a2a2b66f61f2a2e9593290655d7a3cdafc5da
Ashley Chapman <Ash-Bash32@hotmail.co.uk>
2018-12-15 16:31:15 +0000

    Added Sass into webpack script

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== 'production'
 
 const htmlPlugin = new HtmlWebPackPlugin({
     template: __dirname + "/src/html/index.html",
     filename: "./index.html"
 });
 
+const miniCssPlugin = new MiniCssExtractPlugin({
+    // Options similar to the same options in webpackOptions.output
+    // both options are optional
+    filename: "dissue.main.css",
+    publicPath: "/dist"
+});
+
 module.exports = {
     context: __dirname + "/src",
-    entry:  "./js/app.js",
+    entry:  {
+        main: ["./js/app.js", './scss/app.scss']
+    },
     output: {
         path: __dirname + "/dist",
         filename: "dissue.bundle.js"
@@ -18,8 +29,20 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: ['babel-loader']
-            }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
+                    { loader: 'sass-loader', options: { sourceMap: true } },
+                ],
+              }
         ]
     },
-    plugins: [htmlPlugin]
+    plugins: [htmlPlugin, miniCssPlugin],
+    devServer: {  // configuration for webpack-dev-server
+      contentBase: './src/html',  //source of static assets
+      port: 8082, // port to run dev-server
+  } 
 };
